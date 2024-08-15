65bb455a91a35134337af8b644503d751e8fe8df
Defwokin <defwokin@gmail.com>
2024-08-15 19:22:03 +0800

    next commit

---
+++ assets/Card-3mFzXopw.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-DE6QBiCM.js";const t={},r={class:"w-full h-[213px] rounded-[20px] bg-white/10"};function s(n,_){return o(),c("div",r)}const d=e(t,[["render",s]]);export{d as default};
