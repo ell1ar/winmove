1cc54d62674439db5f46eac2fcac89cc4b7ec1b9
Defwokin <defwokin@gmail.com>
2024-08-16 14:52:19 +0800

    next commit

--- assets/List-B1I7DI0J.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,b as s,c as a,d as n,o as c}from"./index-BE8T26pB.js";const r={},t={class:"h-[270px] flex gap-5 p-5 bg-[#101113] rounded-[20px]"};function d(_,p){const e=s("MainBannerCard");return c(),a("div",t,[n(e),n(e,{class:"hidden sm:flex"})])}const l=o(r,[["render",d]]);export{l as default};
