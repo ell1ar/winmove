c9534f5cacf35a0e6af8d1f5cc70a1fa5e3f21e4
Defwokin <defwokin@gmail.com>
2024-08-21 22:06:55 +0800

    next commit

--- src/components/Main/Banner/Card1.vue
+++ src/components/Main/Banner/Card1.vue
@@ -8,22 +8,53 @@
             <p class="text-[22px] uppercase text-white">Бонус</p>
 
             <div class="relative flex flex-nowrap font-['Stapel_Expanded'] text-[70px] text-white">
-                <span>5</span>
-                <span class="relative inline-flex items-center justify-center"
-                    >0 <img class="absolute z-[2] translate-y-0.5" src="./assets/diamond.svg" alt="" />
-                    <span class="absolute z-0 h-10 w-10 translate-y-0.5 rounded-full bg-white"></span>
-                </span>
-                <span class="relative inline-flex items-center justify-center"
-                    >0 <img class="absolute z-[2] translate-y-0.5" src="./assets/diamond.svg" alt="" />
-                    <span class="absolute z-0 h-10 w-10 translate-y-0.5 rounded-full bg-white"></span>
-                </span>
-                <span>%</span>
+                <div class="relative flex items-center justify-center">
+                    <span>5</span>
+                </div>
+                <div class="relative flex items-center justify-center">
+                    <span>0</span>
+                    <img class="absolute z-[2]" src="./assets/diamond.svg" alt="" />
+                    <span class="absolute z-0 h-10 w-10 rounded-full bg-white"></span>
+                </div>
+                <div class="relative flex items-center justify-center">
+                    <span>0</span>
+                    <img class="absolute z-[2]" src="./assets/diamond.svg" alt="" />
+                    <span class="absolute z-0 h-10 w-10 rounded-full bg-white"></span>
+                </div>
+                <div class="relative flex items-center justify-center">
+                    <span>%</span>
+                </div>
             </div>
 
             <p class="font-light text-white">на первый депозит</p>
         </div>
 
         <img class="absolute left-0 top-0 z-[1] h-full w-full object-cover" src="./assets/card2pattern.svg" alt="card-pattern" />
-        <img class="absolute left-0 top-0 z-[1] h-full w-full object-cover" src="./assets/card1light.svg" alt="card-light" />
+        <!-- Gradients -->
+        <svg class="absolute left-0 top-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="203" height="213" viewBox="0 0 203 213" fill="none">
+            <g opacity="0.3" filter="url(#filter0_f_1257_42369)">
+                <ellipse cx="-197.5" cy="120" rx="293.5" ry="206" transform="rotate(180 -197.5 120)" fill="#91E2FD" />
+            </g>
+            <defs>
+                <filter id="filter0_f_1257_42369" x="-597.8" y="-192.8" width="800.6" height="625.6" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="53.4" result="effect1_foregroundBlur_1257_42369" />
+                </filter>
+            </defs>
+        </svg>
+
+        <svg class="absolute bottom-0 right-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="295" height="213" viewBox="0 0 295 213" fill="none">
+            <g opacity="0.5" filter="url(#filter0_f_1257_42370)">
+                <ellipse cx="422" cy="225" rx="242" ry="170" transform="rotate(180 422 225)" fill="#91E2FD" />
+            </g>
+            <defs>
+                <filter id="filter0_f_1257_42370" x="0.399994" y="-124.6" width="843.2" height="699.2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="89.8" result="effect1_foregroundBlur_1257_42370" />
+                </filter>
+            </defs>
+        </svg>
     </div>
 </template>
