da6571dbe61b4b290f4b018850c00344ae025da7
Defwokin <defwokin@gmail.com>
2024-08-21 21:47:35 +0800

    next commit

--- src/components/Main/Game/Widget.vue
+++ src/components/Main/Game/Widget.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="gap-5 rounded-[20px] bg-[#101113] p-5 xl:flex-col">
         <MainGameHead />
-        <MainGameList />
+        <div class="flex">
+            <MainGameList />
+        </div>
     </div>
 </template>
