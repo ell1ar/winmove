c4b55fc57fccec119982ca2ca7b52cec933cafe1
Defwokin <defwokin@gmail.com>
2024-08-22 17:39:18 +0800

    next commit

--- src/components/Main/Banner/Card1.vue
+++ src/components/Main/Banner/Card1.vue
@@ -31,30 +31,7 @@
 
         <img class="absolute left-0 top-0 z-[1] h-full w-full object-cover" src="./assets/card2pattern.svg" alt="card-pattern" />
         <!-- Gradients -->
-        <svg class="absolute left-0 top-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="203" height="213" viewBox="0 0 203 213" fill="none">
-            <g opacity="0.3" filter="url(#filter0_f_1257_42369)">
-                <ellipse cx="-197.5" cy="120" rx="293.5" ry="206" transform="rotate(180 -197.5 120)" fill="#91E2FD" />
-            </g>
-            <defs>
-                <filter id="filter0_f_1257_42369" x="-597.8" y="-192.8" width="800.6" height="625.6" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                    <feGaussianBlur stdDeviation="53.4" result="effect1_foregroundBlur_1257_42369" />
-                </filter>
-            </defs>
-        </svg>
-
-        <svg class="absolute bottom-0 right-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="295" height="213" viewBox="0 0 295 213" fill="none">
-            <g opacity="0.5" filter="url(#filter0_f_1257_42370)">
-                <ellipse cx="422" cy="225" rx="242" ry="170" transform="rotate(180 422 225)" fill="#91E2FD" />
-            </g>
-            <defs>
-                <filter id="filter0_f_1257_42370" x="0.399994" y="-124.6" width="843.2" height="699.2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                    <feGaussianBlur stdDeviation="89.8" result="effect1_foregroundBlur_1257_42370" />
-                </filter>
-            </defs>
-        </svg>
+        <img class="absolute left-0 top-0 z-[1] h-full" src="./assets/card1light1.png" alt="card1light" />
+        <img class="absolute bottom-0 right-0 z-[1] h-full" src="./assets/card1light.png" alt="card1light" />
     </div>
 </template>
--- src/components/Main/Banner/Card2.vue
+++ src/components/Main/Banner/Card2.vue
@@ -19,30 +19,7 @@
         <img class="absolute right-0 top-0 z-10 h-full w-[165px] object-contain lg:w-auto" src="./assets/card1.png" alt="card-1" />
         <img class="absolute left-0 top-0 z-[1] h-full w-full object-cover" src="./assets/card1pattern.svg" alt="card-pattern" />
         <!-- Gradients -->
-        <svg class="absolute left-0 top-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="205" height="221" viewBox="0 0 205 221" fill="none">
-            <g opacity="0.5" filter="url(#filter0_f_1889_125250)">
-                <ellipse cx="-112" cy="106" rx="199" ry="111" transform="rotate(180 -112 106)" fill="#BD52FD" />
-            </g>
-            <defs>
-                <filter id="filter0_f_1889_125250" x="-428.3" y="-122.3" width="632.6" height="456.6" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                    <feGaussianBlur stdDeviation="58.65" result="effect1_foregroundBlur_1889_125250" />
-                </filter>
-            </defs>
-        </svg>
-
-        <svg class="absolute bottom-0 right-0 z-[1] h-full" xmlns="http://www.w3.org/2000/svg" width="384" height="221" viewBox="0 0 384 221" fill="none">
-            <g opacity="0.6" filter="url(#filter0_f_1889_125249)">
-                <ellipse cx="277" cy="196.5" rx="87.5" ry="91" transform="rotate(-90 277 196.5)" fill="#BD52FD" />
-            </g>
-            <defs>
-                <filter id="filter0_f_1889_125249" x="0.399994" y="-76.6002" width="553.2" height="546.2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                    <feGaussianBlur stdDeviation="92.8" result="effect1_foregroundBlur_1889_125249" />
-                </filter>
-            </defs>
-        </svg>
+        <img class="absolute left-0 top-0 z-[1] h-full" src="./assets/card2light1.png" alt="card1light" />
+        <img class="absolute left-0 top-0 z-[1] h-full" src="./assets/card2light.png" alt="card1light" />
     </div>
 </template>
