cd48a2012c17102638722f55817dcb733166bcf3
Defwokin <defwokin@gmail.com>
2024-09-04 23:26:45 +0800

    next commit

---
+++ src/components/Profile/Tabs/Bonuses/Special/NoBanner.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex h-[207px] items-center rounded-[20px] justify-center border-[1px] border-[#191A1C] bg-[#121315]">
+        <span class="text-[18px] text-[#696A6C]">Нет активных предложений</span>
+    </div>
+</template>
